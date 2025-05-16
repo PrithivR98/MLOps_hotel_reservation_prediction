c3d11ae92b7a79455cb26df511349dbffb756a13
PrithivR98 <prithivinboston@gmail.com>
2025-05-16 10:09:57 -0400

    Final Commit

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,18 @@
+from src.data_ingestion import DataIngestion
+from src.data_preprocessing import DataProcesssor
+from src.model_training import ModelTraining
+from utils.common_functions import read_yaml
+from config.paths_config import *
+
+if __name__ =="__main__":
+    ## Data ingestion
+    data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+    data_ingestion.run()
+    
+    ## Data Processing
+    processor = DataProcesssor(TRAIN_FILE_PATH,TEST_FILE_PATH,PROCESSED_DIR,CONFIG_PATH)
+    processor.process() 
+    
+    ## Model training
+    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH,PROCESSED_TEST_DATA_PATH,MODEL_OUTPUT_PATH)
+    trainer.run()
